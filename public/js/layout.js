12d918c054eef23e7fb1a81932ff5996ff043176
Min1RocKer <326800754@qq.com>
2016-06-28 10:18:36 +0800

    V1

---
+++ public/js/layout.js
@@ -0,0 +1,85 @@
+//前端业务处理
+(function(){
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage','thirdPage', 'fourthPage', 'slide1' , 'slide2', 'slide3', 'slide4'],
+
+        afterLoad: function(anchorLink,index){
+            var loadedSection = $(this);
+            //using anchorLink
+            if(anchorLink == 'firstPage'){
+                $('.banner img').animate({
+					top:'50%'
+				},1000,'swing');
+            }
+            if(anchorLink == 'secondPage'){
+                $('.jjLogo img').animate({
+					top:'-60px'
+				},500,'linear');
+            }
+        },
+        onLeave: function(index, nextIndex, direction){
+            //it won't scroll if the destination is the 3rd section
+            if(nextIndex == 1){
+                $('.jjLogo img').animate({
+					top:'-100px'
+				},500,'linear');
+            }
+            if(nextIndex == 2){
+                $('.banner img').animate({
+					top:'40%'
+				},1000,'swing');
+            }
+            if(nextIndex == 3){
+                $('.jjLogo img').animate({
+					top:'-100px'
+				},500,'linear');
+            }
+        },
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+        	var loadedSlide = $(this);
+
+            //first slide of the second section
+            if(anchorLink == 'secondPage' && slideIndex == 0){
+                $('.jjLogo img').animate({
+					top:'-60px'
+				},500,'linear');
+            }
+
+            //second slide of the second section (supposing #secondSlide is the
+            //anchor for the second slide
+            if(index == 2 && slideIndex == 'secondSlide'){
+                alert("Second slide loaded");
+            }
+        },
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+        	var leavingSlide = $(this);
+
+            //leaving the first slide of the 2nd Section to the right
+            if(index == 2 && slideIndex == 0){
+                $('.jjLogo img').animate({
+					top:'-100px'
+				},500,'linear');
+            }
+
+            //leaving the 3rd slide of the 2nd Section to the left
+            if(index == 2 && slideIndex == 2 && direction == 'left'){
+                
+            }
+        }
+
+
+	});	
+	$(document).on('click', '#moveLeft', function(){
+  		$.fn.fullpage.moveSlideRight();
+	});
+	$(document).on('click', '#moveRight', function(){
+  		$.fn.fullpage.moveSlideLeft();
+	});
+    //Slide 自动轮播
+    var t;
+    function autoPlay(){
+        $.fn.fullpage.moveSlideRight();
+        t=setTimeout(autoPlay,5000);
+    }
+    setInterval(autoPlay(),5000);
+})()
---
+++ website.js
@@ -0,0 +1,36 @@
+//-入口文件
+var express = require('express');
+var path = require('path');
+//var mongoose =require('mongoose');
+//复制source对象中的所有属性覆盖到destination对象上，并且返回 destination 对象. 复制是按顺序的, 所以后面的对象属性会把前面的对象属性覆盖掉(如果有重复).
+var _ = require('underscore')
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+//-express web应用开发框架 路由+中间件    中间件是函数 可访问req对象\res对象\next变量
+var website = express();
+
+//连接到数据库jjyuan
+//mongoose.connect('mong odb://localhost/jjyuan');
+
+//-指定放模板文件的目录
+//-指定模板引擎，后续模板文件不需要加后缀，否则反之
+website.set('views','./views/pages');
+website.set('view engine','jade');
+//-应用级中间件
+//-没有挂载路径的中间件，应用的每个请求都会执行该中间件
+//-bodyParser用于解析客户端请求的body中的内容,内部使用JSON编码处理,url编码处理以及对于文件的上传处理.
+website.use(bodyParser.urlencoded({extended: true}));
+website.use(bodyParser.json());
+website.use(express.static(path.join(__dirname,'public')));
+
+website.listen(port);
+console.log("连接成功，端口号为：" + port);
+
+//**********网站路由**********
+
+//index
+website.get('/',function(req,res){
+	res.render('index',{
+		"title":"杭州萌趣科技"
+	})
+})
